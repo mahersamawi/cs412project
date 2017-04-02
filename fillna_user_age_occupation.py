08bba7847e0bc6c00691f92ece4a04da1f29ab02
NeoWu1216 <neo1216.wu@gmail.com>
2017-04-02 17:20:43 -0500

    fillna gender complete

--- fillna_user_age_occupation.py
+++ fillna_user_age_occupation.py
@@ -3,7 +3,7 @@ from scipy.stats import mode
 import pandas as pd
 
 user_df = pd.read_csv('inputs/user.txt')
-#user_df['Gender'] = user_df['Gender'].fillna(mode(user_df['Gender'])[0][0])
+user_df['Gender'] = user_df['Gender'].fillna(mode(user_df['Gender'])[0][0])
 user_df['Occupation'] = user_df['Occupation'].fillna(mode(user_df['Occupation'])[0][0])
 user_df['Occupation'] = user_df['Occupation'].astype(int)
 user_df['Age'] = user_df['Age'].fillna(numpy.mean(user_df['Age']))
