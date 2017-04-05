4ea7e42a131ddf0878eddad613f6474a86957789
Maher Samawi <MaherSamawi@wirelessprv-10-194-139-59.near.illinois.edu>
2017-04-05 17:52:33 -0500

    done with better result2

--- random_trees_trial.py
+++ random_trees_trial.py
@@ -31,7 +31,7 @@ def convert_genres():
             if genre not in g_dict:
                 counter += 1
                 g_dict[genre] = counter
-    print(g_dict)
+    #print(g_dict)
 
     # Change this XXX
 
@@ -94,11 +94,11 @@ def main():
     for index, line in enumerate(test_data):
         find_ID(test_arr, line[1], line[2], index)
 
-    rf = RandomForestClassifier(n_estimators=100)
+    rf = RandomForestClassifier(n_estimators=75)
     rf.fit(training_data, ratings)
     results = rf.predict(test_arr)
 
-    with open('result.txt', 'w') as f:
+    with open('result2.txt', 'w') as f:
         f.write("Id,rating\n")
         for index in range(len(test_data)):
             # f.write(', '.join(map(str,[line[0],results[]]))+'\n')
