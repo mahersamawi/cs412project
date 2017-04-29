0d2435146389d1bc4b11a29b9a83d625e31f366f
Maher Samawi <MaherSamawi@Mahers-MacBook-Air.local>
2017-04-29 15:00:27 -0500

    Re-factored, needs final confirmation

--- bayes.py
+++ bayes.py
@@ -4,108 +4,112 @@ import pandas as pd
 import numpy as np
 
 
-def open_file(index, arr):
+# Opens the file and reads the contents into an array
+def open_file(arg_num, arr_to_populate):
     try:
-        with open(sys.argv[index]) as f:
+        with open(sys.argv[arg_num]) as f:
             for i, line in enumerate(f):
                 if i == 0:
                     continue
-                arr.append(line.strip().split(','))
+                content = line.strip().split(',')
+                arr_to_populate.append(content)
     except:
         print "Unable to open file"
         return
-    f.close()
 
 
-def make_dict(my_dict, data_array):
+# Creates a dictionary for the users and movies
+def make_dict(dict_to_populate, data_array):
     for dat in data_array:
-        my_dict[int(dat[0])] = str(dat[1:])
+        dict_to_populate[int(dat[0])] = str(dat[1:])
 
 
+# Populates the g_dict (genre dictionary)
 def convert_genres():
     dataframe = pd.read_csv('inputs/movie.txt')
-
     counter = 3
     for value in (pd.Series.unique(dataframe['Genre'])):
         for genre in value.split('|'):
             if genre not in g_dict:
                 counter += 1
                 g_dict[genre] = counter
-    # print(g_dict)
-
-    # Change this XXX
 
 
+# Finds the user attributes and the movie attributes
+# Combines them into one entry in the array
 def find_ID(user_id, movie_id, rating):
+    # User part
     for i, string in enumerate(user_dict[int(user_id)][1:-1].split(",")):
         corrected_str = string.strip()[1:-1]
-        addingAt = ratingsdict[rating - 1][i]
+        adding_at = rating_dict[rating - 1][i]
         if i == 0:
             gender = corrected_str[0]
-            addingAt[gender] = addingAt.get(gender, 0) + 1
+            adding_at[gender] = adding_at.get(gender, 0) + 1
         else:
             value_to_add = int(corrected_str)
-            addingAt[value_to_add] = addingAt.get(value_to_add, 0) + 1
-
+            adding_at[value_to_add] = adding_at.get(value_to_add, 0) + 1
+    # Movie part
     for i, string in enumerate(movie_dict[int(movie_id)][1:-1].split(",")):
         corrected_str = string.strip()[1:-1]
-        addingAt = ratingsdict[rating - 1][i + 3]
+        adding_at = rating_dict[rating - 1][i + 3]
         if i == 0:
             year = int(corrected_str)
-            addingAt[year] = addingAt.get(year, 0) + 1
+            adding_at[year] = adding_at.get(year, 0) + 1
         else:
             for genres in corrected_str.split('|'):
                 num = g_dict[genres]
-                addingAt[num] = addingAt.get(num, 0) + 1
+                adding_at[num] = adding_at.get(num, 0) + 1
 
 
+# Used to predict what rating a user will give a movie
+# Similar to find_ID but used to actually predict the rating
+# Returns an array that tells us the probabilities of the user labeling a
+# movie between 1 and 5
+# Bayes Classifier Algorithm
 def find_rating(user_id, movie_id, totals):
-    # Call function to randomly select tuples
-    # Change Ratings Dict
-    # Now we have the tuples, now find the ratings based on the probabilities
-    # Grab more tuples (10 times)
-    # Add up probability vectors and chose best
     probs = np.ones(5)
     for j in range(5):
         for i, string in enumerate(user_dict[int(user_id)][1:-1].split(",")):
             corrected_str = string.strip()[1:-1]
-            getFrom = ratingsdict[j][i]
+            get_from = rating_dict[j][i]
             if i == 0:
                 gender = corrected_str[0]
-                probs[j] *= (getFrom.get(gender, 0) + 1) * \
+                probs[j] *= (get_from.get(gender, 0) + 1) * \
                     1.0 / (totals[j] + 5)
             else:
                 value_to_add = int(corrected_str)
-                probs[j] *= (getFrom.get(value_to_add, 0) + 1) * \
+                probs[j] *= (get_from.get(value_to_add, 0) + 1) * \
                     1.0 / (totals[j] + 5)
 
         for i, string in enumerate(movie_dict[int(movie_id)][1:-1].split(",")):
             corrected_str = string.strip()[1:-1]
-            getFrom = ratingsdict[j][i + 3]
+            get_from = rating_dict[j][i + 3]
             if i == 0:
                 year = int(corrected_str)
-                probs[j] *= (getFrom.get(year, 0) + 1) * 1.0 / (totals[j] + 5)
+                probs[j] *= (get_from.get(year, 0) + 1) * 1.0 / (totals[j] + 5)
             else:
                 for genres in corrected_str.split('|'):
                     num = g_dict[genres]
-                    probs[j] *= (getFrom.get(num, 0) + 1) * \
+                    probs[j] *= (get_from.get(num, 0) + 1) * \
                         1.0 / (totals[j] + 5)
         probs[j] *= totals[j] * 1.0 / sum(totals)
-        probs[j] *= (len(sorted_train[j])* 1.0) / num_lines 
-    
-    global flag
-    if flag == 0:
-        print(probs)
-        flag += 1
+        probs[j] *= (len(sorted_train[j]) * 1.0) / num_lines
     return probs
 
 
-def prob():
-    pass
+# Takes the test data and results arrray
+# Results stores the probabilities of the each label for that example
+# Prints to the file the max of the array entries in results
+def print_results(test_data, results):
+    with open('Bayes_result_final.txt', 'w') as f:
+        f.write("Id,rating\n")
+        for index in range(len(test_data)):
+            f.write("%s,%s\n" %
+                    (test_data[index][0], int(np.argmax(results[index]) + 1)))
 
 
 def main():
-
+    # Genre dictionary
     global g_dict
     g_dict = {}
     convert_genres()
@@ -114,13 +118,13 @@ def main():
     user_data = []
     movie_data = []
     test_data = []
+    # Populate arrays
     open_file(1, training_data_list)
     open_file(2, user_data)
     open_file(3, movie_data)
     open_file(4, test_data)
 
-    global flag
-    flag = 0
+    # Movie and user dictionaries
     global movie_dict
     global user_dict
     movie_dict = {}
@@ -129,29 +133,34 @@ def main():
     make_dict(user_dict, user_data)
     make_dict(movie_dict, movie_data)
 
-    global ratingsdict
-    ratingsdict = [[{} for i in range(5)] for j in range(5)]
+    # Array of dictionaries of size 5 for each label
+    # Ratings dictionary for each attribute for each label
+    global rating_dict
+    rating_dict = [[{} for i in range(5)] for j in range(5)]
+
     totals = [0] * 5
     number_dimension = 22
     training_data = np.zeros((len(training_data_list), number_dimension))
     results = np.zeros((len(test_data), 5))
 
-    global sorted_train 
+    global sorted_train
     sorted_train = [[]for i in range(5)]
 
-    global num_lines
+    # Sort the training data based on the labels
     num_lines = 0
     for line in training_data_list:
         sorted_train[int(line[3]) - 1].append(line)
         num_lines += 1
     num_lines -= 1
 
-
+    # Bagging Method
+    # Pick random label from 0 - 4
+    # Pick random example from label
+    # Totals array stores the sums of each label
     for count in range(30):
-        ratingsdict = [[{} for i in range(5)] for j in range(5)]
+        rating_dict = [[{} for i in range(5)] for j in range(5)]
         for line in training_data_list:
-            # Check if errors
-            ran_out = random.randint(0,4)
+            ran_out = random.randint(0, 4)
             ran_in = random.randint(0, len(sorted_train[ran_out]) - 1)
             s_line = sorted_train[ran_out][ran_in]
 
@@ -161,13 +170,9 @@ def main():
         for index, line in enumerate(test_data):
             results[index] += find_rating(line[1], line[2], totals)
 
-    # Here we'll use our ratingsdict
+    # Output the predicted values
+    print_results(test_data, results)
 
-    with open('Bayes_result_final.txt', 'w') as f:
-        f.write("Id,rating\n")
-        for index in range(len(test_data)):
-            f.write("%s,%s\n" % (test_data[index][0], int(np.argmax(results[index]) + 1 )))
-    
 
 if __name__ == '__main__':
     main()
